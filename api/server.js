999bddb54b740fae1305c63c2f98ada6874179a1
Nicola Richli <nicola@akenza.io>
2024-10-17 17:13:09 +0200

    new version

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 import { build } from "./app.js";
 import cors from "@fastify/cors";
 
-const app = build({ logger: true });
+const app = build({ logger: true, keepAliveTimeout: 600000 });
 app.register(cors);
 
 app.listen({ port: 3000 }, (err, address) => {
